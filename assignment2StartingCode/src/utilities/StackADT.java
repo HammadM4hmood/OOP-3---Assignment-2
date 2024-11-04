d635fc168c284fc6c34a6b501a3e856c161a40ed
LTG-CB <curtis.borson@edu.sait.ca>
2024-11-04 12:48:05 -0700

    Update StackADT.java
    author Hammad Mahmood

--- assignment2StartingCode/src/utilities/StackADT.java
+++ assignment2StartingCode/src/utilities/StackADT.java
@@ -1,26 +1,85 @@
 package utilities;
 
-public interface StackADT<T> {
+public interface StackADT<E> {
 	
-	void push(T element);
+	/*
+	 * Adds an element to top of the stack
+	 * 
+	 * @param element to be added
+	 * */
+	void push(E element);
 	
-	T pop();
+	/*
+	 * removes the top element from the stack
+	 * 
+	 * @return the removed element
+	 * */
+	E pop();
 	
-	T peek();
+	/*
+	 * return the top element from the stack
+	 * 
+	 * @return Element at the top of the stack
+	 * */
+	E peek();
 	
-	boolean isEqual(T element);
+	/*
+	 * compares this stack to another stack to check if they are equal
+	 * 
+	 * @param the stack to compare to
+	 * @return A true if the stacks are equal, otherwise a false
+	 * */
+	boolean isEqual(StackADT<E> that);
 	
-	Iterator<T> iterator();
+	/*
+	 * Iterators through the stack
+	 * 
+	 * @return an iterator through the stack
+	 * */
+	Iterator<E> iterator();
 	
-	T[] toArray(T[] copy);
 	
-	int search(T obj);
+	/*
+	 * returns an array which contains the elements from the stack
+	 * 
+	 * @return Array of the stack
+	 * */
+	Object[] toArray();
 	
-	boolean contains(T obj);
+	/*
+	 * Searches for a an element
+	 * 
+	 * @param the element to be searched for
+	 * 
+	 * @return returns the element closest to the top
+	 * */
+	int search(E obj);
 	
+	/*
+	 * Checks if the stack contains the element
+	 * 
+	 * @param the element to search for
+	 * 
+	 * @return true if the element exist, else false
+	 * */
+	boolean contains(E obj);
+	
+	/*
+	 * returns the size of the stack
+	 * 
+	 * @return size of the stack
+	 * */
 	int size();
 	
+	/*
+	 * Checks if the stack is empty
+	 * 
+	 * @return True if the stack is empty else False
+	 * */
 	boolean isEmpty();
 	
+	/*
+	 * removes all elements from the stack
+	 * */
 	void clear();
 }
